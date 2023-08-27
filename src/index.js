e4c2c8e332fbaa91375154e44b0caf5f42cd8aa1
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-08-27 22:40:58 +0100

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+  const form=document.querySelector("form")
+  form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+  const input=document.querySelector("#searchByID")
+  fetch(`http://localhost:3000/movies/${input.value}`).then(rspn => rspn.json()).then(data => {
+    console.log(data);
+    const titleHeading=document.querySelector("#movieDetails h4")  
+    const summaryParagraph=document.querySelector("#movieDetails p")  
+    titleHeading.textContent=data.title
+    summaryParagraph.textContent=data.summary
+})
+ 
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
